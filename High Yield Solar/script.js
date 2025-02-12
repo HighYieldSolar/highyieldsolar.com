576c70814c5aa2164a0e3437920352aa05721970
HighYieldSolar <info@highyieldsolar.com>
2025-02-11 21:35:34 -0800

    Delete High Yield Solar directory

--- High Yield Solar/script.js
+++
@@ -1,5 +0,0 @@
-// Get the current year
-const currentYear = new Date(2/1/2025).getFullYear(2025);
-
-// Update the footer with the current year
-document.getElementById('footer-year').indexhtml = `&copy; ${currentYear} High Yield Solar. All rights reserved.`;
